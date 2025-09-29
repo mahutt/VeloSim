e992780e1f433267b5f64101066cb0fccaaf378f
mahutt <tommy.mahut@gmail.com>
2025-09-29 15:27:59 -0400

    feat: add station markers to map #13

---
+++ front/app/providers/simulation-provider.tsx
@@ -0,0 +1,71 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 VeloSim Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  type ReactNode,
+} from 'react';
+
+import { useMap } from './map-provider';
+import { MapSource, setMapSource } from '~/lib/map-helpers';
+
+type SimulationContextType = {
+  state: React.RefObject<GeoJSON.GeoJSON | null>;
+};
+
+const SimulationContext = createContext<SimulationContextType | undefined>(
+  undefined
+);
+
+export const SimulationProvider = ({ children }: { children: ReactNode }) => {
+  const { mapRef, mapLoaded } = useMap();
+  const state = useRef<GeoJSON.GeoJSON | null>(null);
+
+  useEffect(() => {
+    if (!mapLoaded) return;
+    fetch('/placeholder-data/stations.geojson')
+      .then((res) => res.json())
+      .then((data) => {
+        state.current = data;
+        setMapSource(MapSource.Stations, data, mapRef.current!);
+      });
+  }, [mapLoaded]);
+
+  return (
+    <SimulationContext.Provider value={{ state }}>
+      {children}
+    </SimulationContext.Provider>
+  );
+};
+
+export const useSimulation = (): SimulationContextType => {
+  const context = useContext(SimulationContext);
+  if (!context) {
+    throw new Error('useSimulation must be used within a SimulationProvider');
+  }
+  return context;
+};
