662ed8fbbb39e3ce4d9cfa5b7a4165d8212fe890
cmezzaca <christophermezzacappa818@yahoo.ca>
2025-10-01 00:38:29 -0400

    feature: Added scafolding for simulator class. #26

--- sim/simulator.py
+++ sim/simulator.py
@@ -21,3 +21,17 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
+
+class Simulator:
+    def start(self) -> None:
+        return
+
+    def stop(self) -> None:
+        return
+
+    def pause(self) -> None:
+        return
+
+    def status(self) -> None:
+        return
