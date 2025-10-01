1b91709774f4235f4fe972d2776f7c187aaee935
Ambrose821 <mclaughlinambrose@gmail.com>
2025-09-30 22:48:01 -0400

    feat: Add basic skeleton for SimulatorController class #36

---
+++ sim/SimulatorController.py
@@ -0,0 +1,57 @@
+"""
+MIT License
+
+Copyright (c) 2025 VeloSim Contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from sim.RealTimeDriver import RealTimeDriver
+import simpy
+import threading
+
+
+class SimulatorController:
+
+    # TODO Add entity Collections. Ex Resources, Stations, Tasks, etc.
+
+    def __init__(
+        self, simEnv: simpy.Environment, realTimeFactor: float, strict: bool
+    ) -> None:
+        self.simEnv = simEnv
+        self.realTimeDriver = RealTimeDriver(simEnv, realTimeFactor, strict)
+        # TODO Initialise frame emitter here
+
+    def start(self, simTime: int) -> None:
+        pass
+
+    def stop(self) -> None:
+        pass
+
+    def pause(self) -> None:
+        self.realTimeDriver.pause()
+
+    def resume(self) -> None:
+        self.realTimeDriver.resume()
+
+    def set_factor(self, factor: float) -> None:
+        self.realTimeDriver.setRealTimeFactor(factor)
+
+    def subscribe_to_frames(self) -> None:
+        pass
