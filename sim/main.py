ac2bf8469a4fc2b5cf3a4d50c2c4775a045bca24
cmezzaca <christophermezzacappa818@yahoo.ca>
2025-10-07 13:34:16 -0400

    feat: fixing up main. #35

--- sim/main.py
+++ sim/main.py
@@ -22,18 +22,53 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+
 # sim/main.py
+from typing import List
+from sim.entities.frame import Frame
 from sim.entities.inputParameters import InputParameter
+from sim.utils.subscriber import Subscriber
 from .simulator import Simulator
 
+
+class LoggerSubscriber(Subscriber):
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def on_frame(self, frame: Frame) -> None:
+        print(f"[{self.name}] Received: {frame}")
+
+
+class WebSocketSubscriber(Subscriber):
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def on_frame(self, frame: Frame) -> None:
+        print(f"[{self.name}] Received: {frame}")
+
+
 params = InputParameter()
+
+
+sim1_sub = WebSocketSubscriber("SIM-1 WebSock")
+sim_1_db_logger = LoggerSubscriber("Database-1_Logger")
+sim_1_error_logger = LoggerSubscriber("Error-1_Logger")
+sub_list_1: List[Subscriber] = [sim1_sub, sim_1_db_logger, sim_1_error_logger]
+
+
+sim2_sub = WebSocketSubscriber("SIM-2 WebSock")
+sim_2_db_logger = LoggerSubscriber("Database-2_Logger")
+sim_2_error_logger = LoggerSubscriber("Error-2_Logger")
+sub_list_2: List[Subscriber] = [sim2_sub, sim_2_db_logger, sim_2_error_logger]
+
 
 if __name__ == "__main__":
     import time
 
     sim = Simulator()
-    r1 = sim.start(params)
-    r2 = sim.start(params)
+
+    r1 = sim.start(params, sub_list_1)
+    r2 = sim.start(params, sub_list_2)
     time.sleep(3)
     sim.stop(r1)
     time.sleep(1)
