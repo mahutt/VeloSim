3137e65db4af593f45997dcfc53a6f0cbd0f972c
Sumer <sumerabdalla@hotmail.com>
2025-10-07 13:43:38 -0400

    feat: Added task and batteryswaptask entities #29

---
+++ sim/entities/BatterySwapTask.py
@@ -0,0 +1,63 @@
+"""
+MIT License
+
+Copyright (c) 2025 VeloSim Contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import simpy
+from typing import Optional
+from .task import Task, State
+from .station import Station
+from .resource import Resource
+
+
+class BatterySwapTask(Task):
+    def __init__(self, env: simpy.Environment, task_id: int, station: Station):
+        super().__init__(env, task_id, station)
+
+    def get_state(self) -> State:
+        return self.state
+
+    def set_state(self, state: State) -> None:
+        self.state = state
+
+    def get_task_id(self) -> int:
+        return self.id
+
+    def get_station(self) -> Station:
+        return self.station
+
+    def get_assigned_resource(self) -> Optional[Resource]:
+        return self.assigned_resource
+
+    def set_assigned_resource(self, resource: Resource) -> None:
+        self.assigned_resource = resource
+        self.state = State.ASSIGNED
+
+    def unassign_resource(self) -> None:
+        self.assigned_resource = None
+        self.state = State.OPEN
+
+    def is_assigned(self) -> bool:
+        if self.state == State.ASSIGNED:
+            return True
+        else:
+            return False
---
+++ sim/entities/task.py
@@ -0,0 +1,81 @@
+"""
+MIT License
+
+Copyright (c) 2025 VeloSim Contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from enum import Enum
+from abc import ABC, abstractmethod
+from typing import Optional
+import simpy
+from .station import Station
+from .resource import Resource
+
+
+# enum of task states
+class State(Enum):
+    OPEN = 1
+    ASSIGNED = 2
+    DISPATCHED = 3
+    CLOSED = 4
+
+
+class Task(ABC):
+    def __init__(self, env: simpy.Environment, task_id: int, station: Station) -> None:
+        self.env = env
+        self.id = task_id
+        self.state = State.OPEN  # A task would always be open at creation
+        self.station = station
+        self.assigned_resource: Optional[Resource] = (
+            None  # Initially, no resource is assigned
+        )
+
+    @abstractmethod
+    def get_state(self) -> State:
+        pass
+
+    @abstractmethod
+    def set_state(self, state: State) -> None:
+        pass
+
+    @abstractmethod
+    def get_task_id(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_station(self) -> Station:
+        pass
+
+    @abstractmethod
+    def get_assigned_resource(self) -> Optional[Resource]:
+        pass
+
+    @abstractmethod
+    def set_assigned_resource(self, resource: Resource) -> None:
+        pass
+
+    @abstractmethod
+    def unassign_resource(self) -> None:
+        pass
+
+    @abstractmethod
+    def is_assigned(self) -> bool:
+        pass
