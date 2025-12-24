9afb969350e1b25572c373c9bba991bc03756062
Giuliano Verdone <giulianoverdone@gmail.com>
2025-12-24 15:04:47 -0500

    fix: scenario template vehicle coordinates #449

--- back/api/v1/scenarios.py
+++ back/api/v1/scenarios.py
@@ -133,7 +133,7 @@ def get_scenario_template(
             "vehicles": [
                 {
                     "name": "Vehicle 1",
-                    "position": [-73.57314, 45.50137],
+                    "position": [-73.561, 45.507],
                     "battery_count": 2,
                 }
             ],
